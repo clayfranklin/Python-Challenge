30bc2fe338717b48197add1570beab98713b0fc3
Clayfranklin <claystudydata@gmail.com>
2019-08-18 16:12:38 -0500

    more code added

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,5 +20,17 @@ with open(csvpath, newline="") as csvfile:
         # Add price
         pnl.append(row[1])
 
-    print (date)
-    print (pnl)
+    #print (date)
+    #print (pnl)
+
+# The total number of months included in the dataset
+    for i in range(len(date)):
+        print("[" + str(i) + "] " + date[i])
+
+# The net total amount of "Profit/Losses" over the entire period
+    #for pnl in csvreader
+     #   sum (pnl)
+      #  print (pnl)
+# The average of the changes in "Profit/Losses" over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
