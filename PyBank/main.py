c8ba8d778fd5cd8ef9a6c45e141cc5828c3dc4c2
Clayfranklin <claystudydata@gmail.com>
2019-08-18 23:35:04 -0500

    more code

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,24 +13,23 @@ pnl = []
 # with open(udemy_csv, newline="", encoding='utf-8') as csvfile:
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    for row in csvreader:
-        # Add title
-        date.append(row[0])
+ # The total number of months included in the dataset  
+    row_count = sum(1 for row in csvreader) 
+    print ("Total Months: " + str(row_count - 1))   
+   
+# this will print all the months with an index value
+    #for i in range(len(date)):
+        #print("[" + str(i) + "] " + date[i])
 
-        # Add price
-        pnl.append(row[1])
-
-    #print (date)
-    #print (pnl)
-
-# The total number of months included in the dataset
-    for i in range(len(date)):
-        print("[" + str(i) + "] " + date[i])
-
-# The net total amount of "Profit/Losses" over the entire period
-    #for pnl in csvreader
-     #   sum (pnl)
-      #  print (pnl)
+# The net total amount of "Profit/Losses" over the entire
+    
+   
+    for row in csvfile:
+        total += float(row[column[1])
+        print ("Total: " + str(total))
+#for pnl in csvreader
+#   sum (pnl)
+#  print (pnl)
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
