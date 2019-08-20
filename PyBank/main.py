233748a3f10873aea3c2b4a33bbb694ae5bf50ed
Clayfranklin <claystudydata@gmail.com>
2019-08-20 10:05:58 -0500

    more code,stuck on sum

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,34 +2,55 @@ import os
 import csv
 
 csvpath = os.path.join("..", "PyBank", "budget_data.csv")
+#def PnL_Total(stuff):
+    #PnL = int(stuff[1])
+    #Date = str(stuff[0])
+
+    #total_months = row_count(Date)
+    #totalPnL = sum(PnL)
+
+    #return totalPnL 
+       
 
 # Open and read csv
-
-date = []
-pnl = []
-
-
-
-# with open(udemy_csv, newline="", encoding='utf-8') as csvfile:
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
- # The total number of months included in the dataset  
-    row_count = sum(1 for row in csvreader) 
-    print ("Total Months: " + str(row_count - 1))   
-   
-# this will print all the months with an index value
-    #for i in range(len(date)):
-        #print("[" + str(i) + "] " + date[i])
-
-# The net total amount of "Profit/Losses" over the entire
     
+ # The total number of months included in the dataset  
+#with open("budget_data.csv") as csvfile:
+    #headerline = next()
+    #total = 0
+    #for row in csv.reader:
+    #    total += int(row[1])
+    #print (total)    
+    
+    #row_count = sum(1 for row in csvreader) 
+    #print ("Total Months: " + str(row_count - 1))   
    
-    for row in csvfile:
-        total += float(row[column[1])
-        print ("Total: " + str(total))
-#for pnl in csvreader
-#   sum (pnl)
-#  print (pnl)
+# The net total amount of "Profit/Losses" over the entire time
+    next(csvreader)
+    for row in csvreader:
+        PnL = [row[1]]
+        x = sum(PnL) 
+        print("Total:  " + int(x))
+
+        
+
+    # make placeholder
+    #store all the values in a list 
+    
+    
+               
+
+    #for row in csvreader:
+        #Date = [row[0]]
+        
+
+
+    #sum all the numbers and print
+    
+
+
 # The average of the changes in "Profit/Losses" over the entire period
 # The greatest increase in profits (date and amount) over the entire period
 # The greatest decrease in losses (date and amount) over the entire period
