cac4c55cffd8e355b226dee66c9f06b5929387aa
Clayfranklin <claystudydata@gmail.com>
2019-08-22 01:27:59 -0500

    almost there

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,27 +2,12 @@ import csv
 import os
 
 csvpath = os.path.join("..", "PyBank", "budget_data.csv")
-#def PnL_Total(stuff):
-    #PnL = int(stuff[1])
-    #Date = str(stuff[0])
-
-    #total_months = row_count(Date)
-    #totalPnL = sum(PnL)
-
-    #return totalPnL 
-       
 
 # Open and read csv
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
- # The total number of months included in the dataset  
-#with open("budget_data.csv") as csvfile:
-    #headerline = next()
-    #total = 0
-    #for row in csv.reader:
-    #    total += int(row[1])
-    #print (total)    
+ # The total number of months included in the dataset     
     
     row_count = sum(1 for row in csvreader) 
     print ("Total Months: " + str(row_count - 1))   
@@ -39,46 +24,40 @@ print(f"Total : {(str(sum(x)))}")
     
 # The average of the changes in "Profit/Losses" over the entire period
 
-
+# create new list with all the values minus the header
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
     diff = []
+    date = []
     for row in csvreader:
         diff.append(int(row[1]))
         x = diff
         
+# create a new list with the differences over each increment
     differences = []
     for i in range(1,len(x)):
         difference_results = x[i] - x[i-1]
         differences.append(difference_results)
-print(differences)
-
-
-
-
-
-
-
-        
-
-    # Python program to get average of a list 
-    #next(csvreader)
-    #Aver = []
-    #for row in csvreader:
-    #    Aver.append(int(row[1]))
-    #    y = Aver
-#print(f"Total Changes : {round(sum(y) / len(y))}")
-
-
-# Driver Code 
-
-
-# Printing average of the list 
+# bart that shit up into a single number to average
+        barf = (str(sum(differences)))
+# print that barfy stew's average
 
 
   
 # The greatest increase in profits (date and amount) over the entire period
 
+# loop through the list and return the highest value
+        high = (max(differences))
+        low = (min(differences))
+
+print("Average Change: " + str(round(int(barf)/85)))
+print(high)
+print(low)
+           
+# find the index of that value
+print(differences.index(high))
+
+# print the index of column [0] with the result from column [1], 
 
 # The greatest decrease in losses (date and amount) over the entire period
