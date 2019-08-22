c5d2e059f29bba18adc046c64ac58c465661083e
Clayfranklin <claystudydata@gmail.com>
2019-08-22 15:16:09 -0500

    first stab at PyPoll

--- PyPoll/mainPoll.py
+++ PyPoll/mainPoll.py
@@ -7,4 +7,41 @@ csvpath = os.path.join("..", "PyPoll", "election_data.csv")
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-print(row[1])
+#The total number of votes cast
+
+    #next(csvreader)
+    #row_count = sum(1 for row in csvreader) 
+    #print ("Total Votes: " + str(row_count - 1)) 
+
+#curious if there is any repeat voter ID's
+    #dup_check =[]
+    #next(csvreader)
+    #for row in csvreader:
+        #dup_check.append(int(row[0]))
+        #a = dup_check
+        #for i in a:
+            #if i in a == i:  
+                #print("There are duplicate voter ID's")
+            #lse:
+                #print( "No duplicate voter ID's!  Election is legit!")
+
+           
+#A complete list of candidates who received votes
+    #turn list into dictionary
+candidates_list = []
+
+file_to_load = os.path.join("..", "PyPoll", "election_data.csv")
+with open(file_to_load) as election_data:
+    reader = csv.reader(election_data)
+
+    header = next(reader)
+    for row in reader:
+        candidates_list = len((row[2]))
+print(candidates_list)
+#The percentage of votes each candidate won
+percentage_of_votes = []
+#The total number of votes each candidate won
+total_votes = []
+#The winner of the election based on popular vote. 
+winner = []
+
