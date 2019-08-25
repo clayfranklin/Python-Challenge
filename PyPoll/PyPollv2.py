415c0283df290a2998cdd396ce7f9f7a67ced753
Clayfranklin <claystudydata@gmail.com>
2019-08-25 00:14:57 -0500

    almost

--- PyPoll/PyPollv2.py
+++ PyPoll/PyPollv2.py
@@ -12,6 +12,7 @@ Khan_votes = 0
 Khan = "Khan"
 total = 0
 winner = 0
+percentage = 0
 
 print("Election Results")
 
@@ -19,7 +20,7 @@ with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
         
-    print("-------------------------")    
+    print("-------------------------")
     for row in csvreader:
         total += 1
 print("Total Votes: " + (str(total)))
@@ -28,22 +29,29 @@ with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
         
-print("-------------------------")    
+    print("-------------------------")
     for row in csvreader:
         Candidate_Name = row[2]
         Candidates.append(Candidate_Name)
-Candidate_Dict = dict(Counter(Candidates))
+    Candidate_Dict = dict(Counter(Candidates))
 
 Khan_percent =Candidate_Dict["Khan"]*100/total
+Khan_percent = round(Khan_percent)
+K_dogPercent = str(Khan_percent)
 Correy_percent = Candidate_Dict["Correy"]*100/total
 Li_percent =Candidate_Dict["Li"]*100/total
-Otooly_percent = Candidate_Dict["O'tooley"]*100/total
+Otooly_percent = Candidate_Dict["O'Tooley"]*100/total
 
+#print("Khan : " + str(Khan_percent) + "%", 
+#for i in Candidate_Dict: 
+    #print(i, Candidate_Dict[i])
 
+print("Khan : " , K_dogPercent , str(Candidate_Dict["Khan"]))
+print("-------------------------")
 
 
 winner = max(Candidate_Dict, key=Candidate_Dict.get)
-print(winner)
+print("Winner : " + winner)
 
 
 #{'Khan': 2218231, 'Correy': 704200, 'Li': 492940, "O'Tooley": 105630}
@@ -51,13 +59,13 @@ print(winner)
 
 #Total Votes: 3521001
 
-  Khan: 63.000% (2218231)
-  Correy: 20.000% (704200)
-  Li: 14.000% (492940)
-  O'Tooley: 3.000% (105630)
-print("-------------------------"
+  #Khan: 63.000% (2218231)
+  #Correy: 20.000% (704200)
+  #Li: 14.000% (492940)
+  #O'Tooley: 3.000% (105630)
+
 #Winner: Khan
-print"-------------------------"
+print("-------------------------")
 
 
 
